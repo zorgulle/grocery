6f3557854ef12c76c051a6c571c0e927a2b25357
Adrien <winy95battle@gmail.com>
2018-07-06 21:22:47 +0200

    Add products creation

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
+from flask import request
 from flask_api import FlaskAPI
 
 app = FlaskAPI(__name__)
 
-@app.route('/products', methods=['GET'])
+PRODUCTS = []
+
+
+def create_product(product):
+    PRODUCTS.append(product)
+
+
+@app.route('/products', methods=['GET', 'POST'])
 def prodcuts():
-    products = [{
-        'id': 1,
-        'ean': 123123123
-    }]
-    return products
+    if request.method == 'POST':
+        create_product(request.json)
+        return PRODUCTS[-1]
+    elif request.method == 'GET':
+        return PRODUCTS
+
 
 @app.route('/', methods=['GET'])
 def index():
     return {'mgs': 'Ok'}
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
