75a3255c2a67b342e5f006f4a109f8c349026780
Adrien <winy95battle@gmail.com>
2018-07-11 21:56:00 +0200

    Product workflow done

--- app.py
+++ app.py
@@ -1,111 +1,72 @@
 """
 This is the entry point of the app
 """
-from flask import request
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 from flask_api import FlaskAPI
+from flask import request
 
 APP = FlaskAPI(__name__)
+engine = sqlalchemy.create_engine('sqlite:///my_db.db')
+session = sessionmaker(bind=engine, autocommit=True)()
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+class Stock(Base):
+    __tablename__='Stock'
+
+    ean = Column(String, primary_key=True)
+    quantity= Column(Integer)
 
 
-class Product:
-    """This is the product Model manager
-    """
-    def __init__(self):
-        self.products = []
+@APP.route('/products/<string:ean>', methods=['DELETE'])
+def delete_product(ean):
+    product = session.query(Stock).filter_by(ean=ean).first()
+    if not product:
+        return {"Error": "No product found"}
 
-    def add(self, product):
-        """Add a products into the model
-        :param product: product to add
-        :return: None
-        """
-        self.products.append(product)
+    product.quantity -= 1
+    product.quantity = max(0, product.quantity)
 
-    def all(self):
-        """Get all products from the model
-        :return: list(dict)
-        """
-        return self.products
+    session.add(product)
+    return {'ean': product.ean, 'quantity': product.quantity}
 
 
-class ProductManager:
-    """Logic
-    """
-    def __init__(self, model):
-        self.model = model
-
-    def add_product(self, product):
-        """This is the loguic of add product
-        :param product: product to add from client
-        :return: product added
-        """
-        self.model.add(product)
-        return self.model.all()[-1]
-
-    def get_product(self, product_id):
-        """Get seach produc with id
-        :param product_id: id of product
-        :return: product with id == product_id
-        """
-        return list(filter(lambda x: x['id'] == product_id, self.model.all()))
-
-    def get_products(self):
-        """Get all products
-        :return: all products
-        """
-        return self.model.all()
-
-    def get_product_with_quantity(self):
-        products = self.model.all()
-        ids = (product['ean'] for product in products)
-        from collections import Counter
-        counter = Counter(ids)
-        return counter
+@APP.route('/products/<string:ean>', methods=['GET'])
+def get_product(ean):
+    product = session.query(Stock).filter_by(ean=ean).first()
+    if not product:
+        return {"Error": "No product found"}
+    return {'ean': product.ean, 'quantity': product.quantity}
 
 
-MODEL = Product()
-PRODUCT_MANAGER = ProductManager(MODEL)
-
-@APP.route('/fridge', methods=['GET'])
-def fridge():
-    """
-    Get products in the fridge
-    :return:
-    """
-    return PRODUCT_MANAGER.get_product_with_quantity()
-
-
-@APP.route('/products/<int:product_id>', methods=['GET'])
-def get_product(product_id):
-    """View of get product
-    :param product_id:
-    :return: list(dict)
-    """
-    return PRODUCT_MANAGER.get_product(product_id)
+@APP.route('/products', methods=['GET'])
+def get_products():
+    products = session.query(Stock).all()
+    return [{'ean': product.ean, 'quantity': product.quantity} for product in products]
 
 
 @APP.route('/products', methods=['POST'])
 def create_product():
-    """View of create products
-    :return: product
-    """
-    product = PRODUCT_MANAGER.add_product(request.json)
-    return product
-
-
-@APP.route('/products', methods=['GET'])
-def products():
-    """view for get all products
-    :return: all products
-    """
-    return PRODUCT_MANAGER.get_products()
-
+    import ipdb; ipdb.set_trace()
+    data = request.json
+    product = session.query(Stock).filter_by(ean=data['ean']).first()
+    if product:
+        product.quantity += 1
+    else:
+        product = Stock(ean=data['ean'], quantity=1)
+    session.add(product)
+    return {'ean': product.ean, 'quantity': product.quantity}
 
 @APP.route('/', methods=['GET'])
 def index():
     """Ping
     :return: dict
     """
-    return {'mgs': 'Ok'}
+    return {'mgs': "ok"}
 
 
 if __name__ == '__main__':
